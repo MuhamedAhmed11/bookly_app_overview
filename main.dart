49b41660a2dee2cca05e6ea7ca082267e338b0e0
MuhamedAhmed11 <mohamed.a.abdelmonem10@gmail.com>
2023-05-24 15:42:50 +0300

    Template file added

---
+++ main.dart
@@ -0,0 +1,6 @@
+// This is just a template file to allow GitHub to detect that this project done
+// with flutter because I can't share the code for confidential and security reasons
+
+var projectName = 'Bookly App';
+var projectDescription =
+    'Mobile app integrated with Google Books API that can perform full-text searches and retrieve book information, viewability and eBook availability. You can also manage your personal bookshelves.';
